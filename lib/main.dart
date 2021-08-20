3785c29172c9b47dd5e71176fb675b933407e660
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-08-19 21:57:50 -0300

    Aplicativo finalizado com ícone personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         backgroundColor: Colors.indigo[300],
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
